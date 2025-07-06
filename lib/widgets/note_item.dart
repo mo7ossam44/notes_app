96689038891bfb010cc4fa1d0e8f2d314e3d584a
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-06 15:17:06 +0300

    create not item container

---
+++ lib/widgets/note_item.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes/models/note_model.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key, required this.noteModel});
+
+  final NoteModel noteModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 24, left: 16, bottom: 24, right: 5),
+      decoration: BoxDecoration(
+        color: noteModel.color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              noteModel.title,
+              style: TextStyle(fontSize: 30, color: Colors.black),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text(
+                noteModel.subtitle,
+                style: TextStyle(
+                  color: Colors.black45,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.delete),
+              color: Colors.black,
+              iconSize: 24,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May20, 2021',
+              style: TextStyle(color: Colors.black45, fontSize: 14),
+            ),
+          ),
+          SizedBox(height: 15),
+        ],
+      ),
+    );
+  }
+}
