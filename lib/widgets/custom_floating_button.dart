7f13760e0bd5094c251fde9dd385058cf6723576
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-06 15:47:21 +0300

    create custom bottom sheet

--- lib/widgets/custom_floating_button.dart
+++ lib/widgets/custom_floating_button.dart
@@ -1,15 +1,20 @@
-
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/add_note_bottom_sheet.dart';
 
 class CustomFloatingActionButton extends StatelessWidget {
-  const CustomFloatingActionButton({
-    super.key,
-  });
+  const CustomFloatingActionButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return const AddNoteButtomSheet();
+          },
+        );
+      },
       child: Icon(Icons.add, color: Colors.black),
       backgroundColor: const Color.fromARGB(255, 120, 255, 241),
       shape: RoundedRectangleBorder(
