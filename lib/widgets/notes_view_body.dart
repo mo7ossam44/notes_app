9ccdcfee900ccb0f8de84cd9b60b247915ef938e
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-06 15:17:55 +0300

    add th custom notes list view builder to my page

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes/widgets/custom_app_bar.dart';
+import 'package:notes/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -8,7 +9,13 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20),
-      child: Column(children: [SizedBox(height: 20), CustomAppBar()]),
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          CustomAppBar(),
+          Expanded(child: NotesListView()),
+        ],
+      ),
     );
   }
 }
