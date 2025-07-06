c9d940213bb3b0aa48999640e3d546af851d8f15
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-06 15:17:24 +0300

    create notes list view builder

---
+++ lib/widgets/notes_list_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notes/models/note_model.dart';
+import 'package:notes/widgets/note_item.dart';
+
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: 15),
+          child: NoteItem(
+            noteModel: NoteModel(
+              color: Colors.yellow.shade200,
+              title: 'Flutter Tips',
+              subtitle: 'build yor career with thrwat same',
+              dateTime: DateTime(2000),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
