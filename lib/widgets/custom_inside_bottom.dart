d1ef08170dca63ff3d4c2799f51096e0eb711e82
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-07 07:56:58 +0300

    add changes in a custom button

--- lib/widgets/custom_inside_bottom.dart
+++ lib/widgets/custom_inside_bottom.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:notes/constants.dart';
 
-class CustomInsideBottom extends StatelessWidget {
-  const CustomInsideBottom({super.key, required this.text});
+class CustomInsideButton extends StatelessWidget {
+  const CustomInsideButton({super.key, required this.text});
 
   final String text;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 165),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 120, 255, 241),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Text(
-          text,
-          style: TextStyle(color: Colors.black54, fontSize: 18),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: GestureDetector(
+        onTap: () {},
+        child: Container(
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          height: 50,
+          decoration: BoxDecoration(
+            color: KPrimaryColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
