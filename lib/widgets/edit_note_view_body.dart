fe41314d7891c12dc4d48e2f2261dd0d902f97c7
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-07 07:58:07 +0300

    craete edit note view body

---
+++ lib/widgets/edit_note_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_app_bar.dart';
+import 'package:notes/widgets/custom_text_field.dart';
+
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        children: const [
+          SizedBox(height: 20),
+          CustomAppBar(text: 'Edit Note', icon: Icons.check),
+          SizedBox(height: 20),
+          CustomTextFiled(text: 'Title', maxlines: 1),
+          CustomTextFiled(text: 'Content', maxlines: 5),
+        ],
+      ),
+    );
+  }
+}
