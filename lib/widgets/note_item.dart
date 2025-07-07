6d06549c08a0875fb3fda23833dc132d9a4bbfbc
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-07 07:58:31 +0300

    create a naivgation inside a note item

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:notes/models/note_model.dart';
+import 'package:notes/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key, required this.noteModel});
@@ -9,48 +9,55 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      padding: EdgeInsets.only(top: 24, left: 16, bottom: 24, right: 5),
-      decoration: BoxDecoration(
-        color: noteModel.color,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Text(
-              noteModel.title,
-              style: TextStyle(fontSize: 30, color: Colors.black),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                noteModel.subtitle,
-                style: TextStyle(
-                  color: Colors.black45,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(
+          context,
+        ).push(MaterialPageRoute(builder: (context) => const EditNoteView()));
+      },
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(top: 24, left: 16, bottom: 24, right: 5),
+        decoration: BoxDecoration(
+          color: noteModel.color,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Text(
+                noteModel.title,
+                style: TextStyle(fontSize: 30, color: Colors.black),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  noteModel.subtitle,
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete),
+                color: Colors.black,
+                iconSize: 24,
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete),
-              color: Colors.black,
-              iconSize: 24,
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                'May20, 2021',
+                style: TextStyle(color: Colors.black45, fontSize: 14),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'May20, 2021',
-              style: TextStyle(color: Colors.black45, fontSize: 14),
-            ),
-          ),
-          SizedBox(height: 15),
-        ],
+            SizedBox(height: 15),
+          ],
+        ),
       ),
     );
   }
