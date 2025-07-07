17217f3d03b71f7745da7aad57011f44a242818f
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-07 07:55:38 +0300

    add the vars

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -2,15 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:notes/widgets/custom_search_container.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.text, required this.icon});
+
+  final String text;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text('Notes', style: TextStyle(fontSize: 32)),
-        CustomSearchContainer(),
+        Text(text, style: const TextStyle(fontSize: 32)),
+        CustomSearchContainer(icon: icon),
       ],
     );
   }
