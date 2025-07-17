9f7c33b9134f26dfd2009e4fadd0c997ccd03c7f
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-17 11:28:28 +0300

    make notes in note item

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/notes_cubit/notes_cubit_cubit.dart';
 import 'package:notes/models/note_model.dart';
 import 'package:notes/widgets/note_item.dart';
 
@@ -9,21 +11,22 @@ class NotesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        itemCount: 5,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: NoteItem(
-              noteModel: NoteModel(
-                color: 20,
-                title: 'Flutter Tips',
-                subtitle: 'build yor career with thrwat same',
-                dateTime: DateTime.now().toString(),
-              ),
-            ),
-          );
+      child: BlocBuilder<NotesCubt, NotesCubitState>(
+        builder: (context, state) {
+          List<NoteModel> notes =
+              BlocProvider.of<NotesCubt>(context).notes ?? [];
+            return ListView.builder(
+              padding: EdgeInsets.zero,
+              itemCount: notes.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: NoteItem(notes: notes[index]),
+                );
+              },
+            );
+         
+        
         },
       ),
     );
