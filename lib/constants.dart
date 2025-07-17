9e2b50a67fa00783a729d40ef1a146e2c2bf2cf8
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-17 11:23:50 +0300

    add constants color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,11 @@
 import 'dart:ui';
 
-const kPrimaryColor =  Color(0xff62FCD7);
+const kPrimaryColor = Color(0xff62FCD7);
 const kNoteBox = 'notes_box';
+List<Color> kColors = const [
+  Color(0xffEAC435),
+  Color(0xff345995),
+  Color(0xffE40066),
+  Color(0xff03CEA4),
+  Color(0xffFB4D3D),
+];
