7945e4f21a3c93fe3bc4555a63fc5d89b7352388
Mohamed Hossam <m.mansi2hossam@gmail>
2025-07-17 11:25:18 +0300

    add defulat color and give the color value to the note

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:notes/constants.dart';
 import 'package:notes/models/note_model.dart';
@@ -6,7 +7,10 @@ import 'package:notes/cubits/add_note_cubit/add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+
+  Color color = Color(0xffE40066);
   addNote(NoteModel note) async {
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNoteBox);
